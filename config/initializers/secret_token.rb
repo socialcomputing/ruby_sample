00ba4953b45d2ad0bff4c69ee919c0e7a48ca973
grundprinzip <grundprinzip@gmail.com>
2013-10-24 13:15:16 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocCompBase::Application.config.secret_key_base = 'f3ae35bc551c3ee085e39fa0e8478be25d026e070b7aff6d510cd35c0c11cafb754df573abd9a05c02758be9c57b47f9be35d93215c0755e7392fe42a0658905'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocCompBase::Application.config.session_store :cookie_store, key: '_soc_comp_base_session'
